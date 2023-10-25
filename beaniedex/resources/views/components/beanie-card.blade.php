3c97a8c65d2ecb2504e83a9eb38639fc2f82d482
Jeffrey Bee <jeffreypbee@gmail.com>
2023-10-24 21:36:52 -0400

    styling beanie cards

--- beaniedex/resources/views/components/beanie-card.blade.php
+++ beaniedex/resources/views/components/beanie-card.blade.php
@@ -3,5 +3,9 @@
 <div class="card beanie-card">
     <a href="/beanies/{{$beanie->id}}">
         <img src="{{$beanie->image}}" alt="{{$beanie->name}}" title="{{$beanie->name}}">
+        <div class="info">
+            <div class="name">{{ $beanie->name }}</div>
+            {{-- <div>(Variant Data)</div> --}}
+        </div>
     </a>
 </div>
