236ad44ad4a33fc7689b30692ee70ac75dd35984
Jeffrey Bee <jeffreypbee@gmail.com>
2023-10-24 23:10:28 -0400

    Stylin

--- beaniedex/resources/views/components/beanie-card.blade.php
+++ beaniedex/resources/views/components/beanie-card.blade.php
@@ -1,11 +1,13 @@
 @props(['beanie'])
 
-<div class="card beanie-card" style="background: linear-gradient(160deg, transparent 70%, var(--pink-lucent)), no-repeat center url('{{$beanie->image}}'); background-size: contain;">
-    <a href="/beanies/{{$beanie->id}}">
+<a href="/beanies/{{$beanie->id}}">
+<div class="card beanie-card" style="background: no-repeat top url('{{$beanie->image}}'); background-size: contain;">
+    
         {{-- <img src="{{$beanie->image}}" alt="{{$beanie->name}}" title="{{$beanie->name}}"> --}}
         <div class="info">
             <div class="name">{{ $beanie->name }}</div>
-            {{-- <div>(Variant Data)</div> --}}
+            <div class="variant">(Variant Data)</div>
         </div>
-    </a>
+    
 </div>
+</a>
