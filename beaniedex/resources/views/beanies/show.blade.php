2d38f78103a881e15165859b6055ca4422ada8e1
Jeffrey Bee <jeffreypbee@gmail.com>
2023-10-26 12:16:29 -0400

    Styling Beanie/Show

--- beaniedex/resources/views/beanies/show.blade.php
+++ beaniedex/resources/views/beanies/show.blade.php
@@ -1,28 +1,46 @@
 <x-layout>
-<div class="show-beanie" style="background: no-repeat center url('{{$beanie->image}}')">
+<div 
+    class="show-beanie" 
+    style="background: no-repeat center url('{{$beanie->image}}');
+        background-size: 250%;">
+
     <div class="blur-bg flex-column">
         <div class="beanie-name">
             <h2>
                 {{ $beanie->name }}
             </h2>
-            the {{ $beanie->species }}
+            
+            
+        </div>
+        <h3>the {{ $beanie->species }}</h3>
+        <div class="beanie-info">
+            <img src="{{$beanie->image}}" alt="">
+            <div class="beanie-stats">            
+                <p><a href="/productlines/{{$beanie->productLine->id}}">{{ $beanie->productLine->name }}</a></p>
+                <p>Birthday: {{ $beanie->birthday }}</p>
+                <p>Released: {{ $beanie->releaseDate }}</p>
+                <p>Retired: {{ $beanie->retiredDate }}</p>
+            </div>
+        </div>        
+        <div>
+            <h3>Poem</h3>
+            <div class="poem">
+                {!! nl2br(e($beanie->poem)); !!}
+            </div>            
         </div>
         <div>
-            <img src="{{$beanie->image}}" alt="">
+            <h3>Notes</h3>
+            {{ $beanie->notes }}
         </div>
-        <div class="flex-column">
-            
-            <p><a href="/productlines/{{$beanie->productLine->id}}">{{ $beanie->productLine->name }}</a></p>
-            <p class="poem">
-                {!! nl2br(e($beanie->poem)); !!}
-            </p>
+        <div class="buttons">
+            <a href="/beanies/{{$beanie->id}}/edit">Edit</a>
+            <form method="POST" action="/beanies/{{$beanie->id}}">
+                @csrf
+                @method('DELETE')
+                <button>Delete</button>
+            </form>
         </div>
-        <a href="/beanies/{{$beanie->id}}/edit">Edit</a>
-        <form method="POST" action="/beanies/{{$beanie->id}}">
-            @csrf
-            @method('DELETE')
-            <button>Delete</button>
-        </form>
+        
     </div>    
 </div>
 
