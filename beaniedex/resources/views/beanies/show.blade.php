090116984d037956f833d8c31ca860aac6f0d117
Jeffrey Bee <jeffreypbee@gmail.com>
2023-10-27 08:55:39 -0400

    Stylin

--- beaniedex/resources/views/beanies/show.blade.php
+++ beaniedex/resources/views/beanies/show.blade.php
@@ -9,24 +9,22 @@
             <h2>
                 {{ $beanie->name }}
             </h2>
-            
-            
         </div>
-        <h3>the {{ $beanie->species }}</h3>
         <div class="beanie-info">
             <img src="{{$beanie->image}}" alt="">
             <div class="beanie-stats">            
-                <p><a href="/productlines/{{$beanie->productLine->id}}">{{ $beanie->productLine->name }}</a></p>
+                <p>Product: <a href="/productlines/{{$beanie->productLine->id}}">{{ $beanie->productLine->name }}</a></p>
+                <p>Species: {{ $beanie->species }}</p>
                 <p>Birthday: {{ $beanie->birthday }}</p>
                 <p>Released: {{ $beanie->releaseDate }}</p>
                 <p>Retired: {{ $beanie->retiredDate }}</p>
             </div>
         </div>        
-        <div>
+        <div class="beanie-poem">
             <h3>Poem</h3>
-            <div class="poem">
+            <p class="poem">
                 {!! nl2br(e($beanie->poem)); !!}
-            </div>            
+            </p>            
         </div>
         <div>
             <h3>Notes</h3>
