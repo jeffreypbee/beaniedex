b9d6d87e510d2afba10a3d68f995ea8a898a253c
Jeffrey Bee <jeffreypbee@gmail.com>
2023-10-31 12:52:40 -0400

    Updating Beanie card

--- beaniedex/resources/views/components/beanie-card.blade.php
+++ beaniedex/resources/views/components/beanie-card.blade.php
@@ -1,12 +1,21 @@
 @props(['beanie'])
 
 <a href="/beanies/{{$beanie->id}}">
+    {{-- <div class="beanie-card"> --}}
 <div class="beanie-card" style="background: no-repeat top url('{{$beanie->image}}'); background-size: contain;">
-    
-    {{-- <img src="{{$beanie->image}}" alt="{{$beanie->name}}" title="{{$beanie->name}}"> --}}
+    <div class="productline"
+        style="background-color: {{$beanie->productLine->color}};">
+        {{ $beanie->productLine->name }}
+    </div>
+
+    {{-- <img src="{{$beanie->image}}" 
+        alt="{{$beanie->name}}" 
+        title="{{$beanie->name}}" /> --}}
+
     <div class="info">
         
         <div class="name">{{ $beanie->name }}</div>
+        <div class="version">{{ $beanie->version }}</div>
         
     </div>
     
